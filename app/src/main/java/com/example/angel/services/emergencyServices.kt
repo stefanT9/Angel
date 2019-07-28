6da00fa202536cc7b3288ae6eade331677dd2475
stefanT9 <stefanT9@users.noreply.github.com>
2019-07-28 06:43:06 -0700

    some refactoring

---
+++ app/src/main/java/com/example/angel/services/emergencyServices.kt
@@ -0,0 +1,14 @@
+package com.example.angel.services
+
+import androidx.core.content.ContextCompat.startActivity
+import android.content.Intent
+import android.net.Uri
+
+
+class emergencyServices()
+{
+    fun callEmergency()
+    {
+        val intent=Intent(this,)
+    }
+}
